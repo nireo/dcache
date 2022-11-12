b4d5d7b0b1ae64b853f393cc99f288620cebb089
nireo <eemil.lehtikangas1@gmail.com>
2022-11-12 14:25:01 +0200

    added a server picker based on action

---
+++ server/picker.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"strings"
+	"sync"
+	"sync/atomic"
+
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+)
+
+type Picker struct {
+	sync.RWMutex
+	leader    balancer.SubConn
+	followers []balancer.SubConn
+	curr      uint64
+}
+
+func (p *Picker) Build(buildInfo base.PickerBuildInfo) balancer.Picker {
+	p.Lock()
+	defer p.Unlock()
+
+	var followers []balancer.SubConn
+	for sc, scInfo := range buildInfo.ReadySCs {
+		isLeader := scInfo.Address.Attributes.Value("is_leader").(bool)
+		if isLeader {
+			p.leader = sc
+			continue
+		}
+
+		followers = append(followers, sc)
+	}
+
+	p.followers = followers
+
+	return p
+}
+
+func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	p.RLock()
+	defer p.RUnlock()
+
+	var res balancer.PickResult
+	if strings.Contains(info.FullMethodName, "Set") || len(p.followers) == 0 {
+		res.SubConn = p.leader
+	} else if strings.Contains(info.FullMethodName, "Get") {
+		res.SubConn = p.nextFollower()
+	}
+
+	if res.SubConn == nil {
+		return res, balancer.ErrNoSubConnAvailable
+	}
+
+	return res, nil
+}
+
+func (p *Picker) nextFollower() balancer.SubConn {
+	cur := atomic.AddUint64(&p.curr, uint64(1))
+	len := uint64(len(p.followers))
+	idx := int(cur % len)
+
+	return p.followers[idx]
+}
--- server/server_test.go
+++ server/server_test.go
@@ -11,7 +11,11 @@ import (
 	"github.com/nireo/dcache/server"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/attributes"
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
 )
 
 func setupTest(t *testing.T, fn func(server.Cache)) (
@@ -62,4 +66,81 @@ func TestSetGet(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, []byte("testvalue"), res.Value)
+}
+
+// testConnection
+type subConn struct {
+	addrs []resolver.Address
+}
+
+func (s *subConn) UpdateAddresses(addrs []resolver.Address) {
+	s.addrs = addrs
+}
+
+func (s *subConn) Connect() {}
+
+func setupPickerTest() (*server.Picker, []*subConn) {
+	var subConns []*subConn
+	buildInfo := base.PickerBuildInfo{
+		ReadySCs: make(map[balancer.SubConn]base.SubConnInfo),
+	}
+	for i := 0; i < 3; i++ {
+		sc := &subConn{}
+		addr := resolver.Address{
+			Attributes: attributes.New("is_leader", i == 0),
+		}
+		sc.UpdateAddresses([]resolver.Address{addr})
+		buildInfo.ReadySCs[sc] = base.SubConnInfo{Address: addr}
+		subConns = append(subConns, sc)
+	}
+
+	picker := &server.Picker{}
+	picker.Build(buildInfo)
+	return picker, subConns
+}
+
+func TestPickerNoSubConnAvailable(t *testing.T) {
+	picker := &server.Picker{}
+	for _, method := range []string{
+		"/cache.v1.Cache/Set",
+		"/cache.v1.Cache/Get",
+	} {
+		info := balancer.PickInfo{
+			FullMethodName: method,
+		}
+
+		res, err := picker.Pick(info)
+		require.Nil(t, res.SubConn)
+		require.Equal(t, balancer.ErrNoSubConnAvailable, err)
+	}
+}
+
+func TestPickerChoosesLeader(t *testing.T) {
+	picker, subConns := setupPickerTest()
+	info := balancer.PickInfo{
+		FullMethodName: "/cache.v1.Cache/Set",
+	}
+
+	// try multiple times to ensure that leader is always selected.
+	for i := 0; i < 10; i++ {
+		pick, err := picker.Pick(info)
+
+		require.NoError(t, err)
+		require.Equal(t, subConns[0], pick.SubConn)
+	}
+}
+
+func TestPickerUsesFollowers(t *testing.T) {
+	picker, subConns := setupPickerTest()
+	info := balancer.PickInfo{
+		FullMethodName: "/cache.v1.Cache/Get",
+	}
+
+	// try multiple times to ensure that leader is always selected.
+	for i := 0; i < 10; i++ {
+		pick, err := picker.Pick(info)
+
+		require.NoError(t, err)
+		require.Equal(t, subConns[i%2+1], pick.SubConn)
+	}
 }
